0c6944e206297a6f01d2ba333718bf8b521c791f
Borewit <borewit@users.noreply.github.com>
2022-03-12 20:46:56 +0100

    Add CommonJS TypeScript example

--- ts-commonjs/index.ts
+++ ts-commonjs/index.ts
@@ -1,10 +1,10 @@
-const path = require('path');
+import * as path from 'path';
 
 /**
  * Import 'file-type' ES-Module in CommonJS Node.js module
  */
 (async () => {
-  const { fileTypeFromFile } = await import('file-type');
+  const { fileTypeFromFile } = await (eval('import("file-type")') as Promise<typeof import('file-type')>);
 
   const type = await fileTypeFromFile(path.join('..', 'fixture', 'fixture.gif'));
   console.log(type);
