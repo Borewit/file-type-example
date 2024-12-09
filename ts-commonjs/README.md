d00da7822c5bceed5eeb6c0944e51d4a4c48f10c
Borewit <Borewit@users.noreply.github.com>
2024-12-09 11:34:31 +0100

    Update CommonJS TypeScript module: Load ESM module using `load-esm`

---
+++ ts-commonjs/README.md
@@ -0,0 +1,3 @@
+# ts-commonjs
+
+Demonstrates how file-type can be loaded on CommonJS TypeScript module.
--- ts-commonjs/index.ts
+++ ts-commonjs/index.ts
@@ -1,10 +1,11 @@
 import * as path from 'path';
+import {loadEsm} from 'load-esm';
 
 /**
  * Import 'file-type' ES-Module in CommonJS Node.js module
  */
 (async () => {
-  const { fileTypeFromFile } = await (eval('import("file-type")') as Promise<typeof import('file-type')>);
+  const { fileTypeFromFile } = await loadEsm<typeof import('file-type')>('file-type');
 
   const type = await fileTypeFromFile(path.join('..', 'fixture', 'fixture.gif'));
   console.log(type);
