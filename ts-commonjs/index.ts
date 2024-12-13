dcf51307d6773b98623273bb43af392303361466
Borewit <Borewit@users.noreply.github.com>
2024-12-13 11:23:40 +0100

    Add Typings to CommonJS/TypeScript example

--- ts-commonjs/index.ts
+++ ts-commonjs/index.ts
@@ -5,7 +5,7 @@ import {loadEsm} from 'load-esm';
  * Import 'file-type' ES-Module in CommonJS Node.js module
  */
 (async () => {
-  const { fileTypeFromFile } = await loadEsm('file-type');
+  const { fileTypeFromFile } = await loadEsm<typeof import('file-type')>('file-type');
 
   const type = await fileTypeFromFile(path.join('..', 'fixture', 'fixture.gif'));
   console.log(type);
